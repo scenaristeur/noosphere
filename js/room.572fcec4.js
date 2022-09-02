195bebc25e1cd8eea18e65476ac22ac77d02c399
scenaristeur <scenaristeur@gmail.com>
2022-09-02 18:52:57 +0200

    share

--- js/room.572fcec4.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunknoosphere"]=self["webpackChunknoosphere"]||[]).push([[605],{8609:function(e,n,o){o.r(n),o.d(n,{default:function(){return l}});var s=function(){var e=this,n=e._self._c;return n("div",{staticClass:"room-view"},[e._v(" Room ")])},r=[],t={name:"RoomView"},u=t,c=o(1001),i=(0,c.Z)(u,s,r,!1,null,"32ceec98",null),l=i.exports}}]);
-//# sourceMappingURL=room.572fcec4.js.map
