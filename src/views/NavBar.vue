3882225cf58275bc5ea6b7e68f4c51833aa92038
scenaristeur <scenaristeur@gmail.com>
2022-10-13 03:00:22 +0200

    bug awareness

--- src/views/NavBar.vue
+++ src/views/NavBar.vue
@@ -31,7 +31,7 @@
             <b-dropdown-item href="https://matrix.to/#/#noosphere:matrix.org" target="_blank"> Chat about Noosphere <b-icon icon="chat" aria-hidden="true"></b-icon> </b-dropdown-item>
             <b-dropdown-item href="#">
               <i><small>
-                version: graph
+                version: bug awareness
               </small></i>
             </b-dropdown-item>
           </b-nav-item-dropdown>
